1a98e9a7a02b6b78108b76377b9d5c1c3d670fdc
Ateeq1997 <137155654+Ateeq1997@users.noreply.github.com>
2025-11-20 18:02:26 +0500

    changes1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,9 @@ module.exports = {
     },
     container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+plugins: [
+  require("tailwindcss-animate"),
+],
+
   darkMode: ["class"],
 };
