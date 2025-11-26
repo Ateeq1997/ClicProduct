72c807091d6b76e7de8961515e24090ccb455f64
Ateeq1997 <137155654+Ateeq1997@users.noreply.github.com>
2025-11-26 09:01:51 +0500

    Some changes in images

--- client/src/components/WoodWebDevelopment/MarbleWebsiteSection.jsx
+++ client/src/components/WoodWebDevelopment/MarbleWebsiteSection.jsx
@@ -58,7 +58,7 @@ const MarbleWebsiteSection = () => {
             transition={{ duration: 0.4, ease: "easeOut" }}
             className="absolute top-0 sm:top-0 left-[80px] sm:left-[143px] w-[305px] h-[400px] object-cover rounded-md shadow-lg"
             alt="Design example 2"
-            src="figmaAssets/w7.png"
+            src="figmaAssets/w3.jpg"
           />
         </div>
       </div>
